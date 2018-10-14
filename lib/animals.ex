54f5db383087ac2012f6236722be3124368407e9
JSn1nj4 <e.j.derhay@gmail.com>
2018-10-13 20:06:06 -0400

    Replace Hello World function with some others
    New functions:
    - create_zoo: return list of animals
    - randomize: shuffle a given "zoo" (list of animals)
    - contains?: check if an animal lives in a given zoo

--- lib/animals.ex
+++ lib/animals.ex
@@ -4,15 +4,43 @@ defmodule Animals do
   """
 
   @doc """
-  Hello world.
+  create_zoo returns a list of zoo animals
+
+  ## Examples
+    iex> Animals.create_zoo
+    ["lion", "tiger", "gorilla", "elephant", "monkey", "giraffe"]
+
+  """
+  def create_zoo do
+      ["lion", "tiger", "gorilla", "elephant", "monkey", "giraffe"]
+  end
+
+  @doc """
+  randomise takes a list of zoo animals and returns a new randomised list with
+  the same elements as the first.
 
   ## Examples
 
-      iex> Animals.hello
-      :world
+    iex> zoo = Animals.create_zoo
+    iex> Animals.randomize(zoo)
+    ["monkey", "tiger", "elephant", "gorilla", "giraffe", "lion"]
 
   """
-  def hello do
-    :world
+  def randomize(zoo) do
+      Enum.shuffle(zoo)
+  end
+
+  @doc """
+  contains? takes a list of zoo animals and a single animal and returns a boolean
+  as to whether or not the list contains the given animal.
+
+  ## Examples
+
+    iex> zoo = Animals.create_zoo
+    iex> Animals.contains?(zoo, "gorilla")
+    true
+  """
+  def contains?(zoo, animal) do
+      Enum.member?(zoo, animal)
   end
 end
